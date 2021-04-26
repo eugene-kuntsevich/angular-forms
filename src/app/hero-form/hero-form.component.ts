424f62878bfe3bd4aab55a23d45c30101aacf3fd
Eugene Kuntsevich <eugene.kuntsevich@intexsoft.by>
2021-04-26 13:23:33 +0300

    refactoring

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -5,18 +5,22 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
-  styleUrls: ['./hero-form.component.css']
+  styleUrls: ['./hero-form.component.css'],
 })
 export class HeroFormComponent {
 
-  powers = ['Really Smart', 'Super Flexible',
-    'Super Hot', 'Weather Changer'];
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  constructor() {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
 
   newHero() {
     this.model = new Hero(42, '', '');
